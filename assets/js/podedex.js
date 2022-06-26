c9110d1c4a7aac5aa0394ac99a24ac3cf460b202
Alejandro Migueles <alexmigueles@gmail.com>
2022-06-26 15:45:38 -0500

    chore: se añade fondo para las tarjetas y dos botones al html para que sirvan de paginación

--- assets/js/podedex.js
+++ assets/js/podedex.js
@@ -1,23 +1,28 @@
+//el querySelector funciona usando #ID´s o Clases del css
+//la sintaxis es: document.querySelector(selector)
+//donde el selector es una cadena de texto que indica el elemento que queremos buscar
+//Por ejemplo: document.querySelector('#id') busca el elemento con el id indicado
+//Por ejemplo: document.querySelector('.class') busca el elemento con la clase indicada
 const mainDiv = document.querySelector('#container-pokemons');
 const seachElement = document.querySelector('#search');
 const NOT_IMAGE_TEXT = 'la imagen del pokemon';
-
+let region="https://pokeapi.co/api/v2/pokemon/?limit=3";
 let globalPokemons = [];
 
+//Esta funcion limpia el input de busqueda
 const cleanView = () => {
     mainDiv.innerHTML = ''
     //globalPokemons = []; // cuando inserto esta funcion también elimina el render pokemon
     //entonces es necesario crear una nueva función que las elimine en ordenes distintos
 }
 
- const cleanRenderCardPokemon=  () => {
-     globalPokemons = [];
- }
+//esta funcion limpia el array que contiene los pokemons a mostrar
+const cleanRenderCardPokemon=  () => {
+    globalPokemons = [];
+}
 
-
-
-
-const searchWithFilter = (searchingText) => { //esta funcion es la que busca a los pokemons	y los muestra
+//esta funcion es la que busca a los pokemons	y los muestra
+const searchWithFilter = (searchingText) => {
     const filteredPokemon = globalPokemons.filter((pokemon) => {
         const { name } = pokemon;
         if (name.includes(searchingText)) {
@@ -27,8 +32,8 @@ const searchWithFilter = (searchingText) => { //esta funcion es la que busca a l
     return filteredPokemon;
 }
 
-
-
+//usa el evento keyup para buscar los pokemons
+//keyup es un evento que se activa cuando se suelta una tecla presionada
 seachElement.addEventListener('keyup', (event) => {
     const inputText = event?.target?.value || '';
     let pokemosGlobal2 =  [ ...globalPokemons ]; // globalPokemons.slice(0, globalPokemons.length)
@@ -36,6 +41,8 @@ seachElement.addEventListener('keyup', (event) => {
     cleanView();
     renderPokemons(pokemosGlobal2);
 });
+
+
 
 const getPokemons = async () => { /*obtiene los datos */
     const response= await fetch(region);
@@ -48,26 +55,39 @@ const getPokemons = async () => { /*obtiene los datos */
     };
 };
 
+
 const normalizePokemonData =  (name, imgResponseJson) => {
     const img = imgResponseJson?.sprites?.other['official-artwork']?.front_default || '';
-    const pokemon = { name: name, img: img };
+    const pokemon = { name: name, img: img};
     globalPokemons.push(pokemon);
 };
 
 const renderCardPokemon = (element, index) => {
 
+    //esta porción de código crea los elementos de la tarjeta para despues poder insertarlos al html
     const cardPokemonDiv = document.createElement('button');
+    const pokemonImgDiv= document.createElement('div');
     const pokemonImg = document.createElement('img');
     const brElement = document.createElement('br');
     const pokemonNameSpan = document.createElement('span');
 
-    cardPokemonDiv.className = 'center';
+    //este código brinda las clases a los elementos creados anteriormente
+    cardPokemonDiv.className = 'centerCard';
+    pokemonImgDiv.className = 'pokemonImgDiv';
     pokemonImg.className = 'icon-region';
+
+    //brinda un atributo al elemento seleccionado, en este ccaso el pokemonImg
+    //la sintaxis nos dice Element.setAttribute(name(del atributo), value);
+    //los atributos validos son: name, src,
     pokemonImg.setAttribute('src', element.img);
     pokemonImg.setAttribute('alt', NOT_IMAGE_TEXT);
 
+    //esta porción de código inserta los elementos creados anteriormente al html en cascada (Uno dentro del otro)
     mainDiv.appendChild(cardPokemonDiv);
-    cardPokemonDiv.appendChild(pokemonImg);
+
+    cardPokemonDiv.appendChild(pokemonImgDiv);
+    
+    pokemonImgDiv.appendChild(pokemonImg);
     cardPokemonDiv.appendChild(brElement);
     cardPokemonDiv.appendChild(pokemonNameSpan);
     pokemonNameSpan.innerHTML = element.name;
@@ -85,12 +105,8 @@ async function main() {
     renderPokemons(globalPokemons);
 }
 
-
-
-
-let region="https://pokeapi.co/api/v2/pokemon/?limit=3";
+//esta funcion se encarga de cambiar el region de los pokemons de acuerdo al boton seleccionado
 const boton=document.querySelectorAll('button');
-
 boton.forEach(function (item){
     console.log(item);
     item.addEventListener('click', function(){
@@ -134,4 +150,14 @@ boton.forEach(function (item){
 )
 
 
+
 main();
+
+//Esta es una funcion de prueba para entender como está organizada la informacion de la api
+function fetchPokemon(id) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+fetchPokemon(4)
